3fc1c79a215c22ea3fd2858f5b17092776d929f1
Felipe Frison <fcfrison@gmail.com>
2025-07-09 20:14:56 -0300

    implement parse torrent file

---
+++ app/decoders.go
@@ -0,0 +1,202 @@
+package main
+
+import (
+	"errors"
+	"strconv"
+	"unicode"
+	// bencode "github.com/jackpal/bencode-go" // Available if you need it!
+)
+
+const (
+	BencodeInteger = iota
+	BencodeList
+	BencodeDict
+	BencodeString
+	BencodeInvalid
+)
+
+func decodeBencode(bencodedString string) (any, error) {
+	if len(bencodedString) == 0 {
+		return 0, errors.New("decode error: the string is empty")
+	}
+	pos := 0
+	var value any
+	var err error
+	for pos < len(bencodedString) {
+		value, err = _decodeBencode(&pos, bencodedString)
+		if err != nil {
+			return value, err
+		}
+		pos++
+	}
+	return value, err
+}
+func _decodeBencode(pos *int, bencodedString string) (any, error) {
+	switch findOutBencodeType(rune(bencodedString[*pos])) {
+	case BencodeString:
+		return decodeString(pos, bencodedString)
+	case BencodeInteger:
+		return decodeInteger(pos, bencodedString)
+	case BencodeList:
+		return decodeList(pos, bencodedString)
+	case BencodeDict:
+		return decodeDictionary(pos, bencodedString)
+	default:
+		break
+	}
+	return "", errors.New("decode error: invalid character")
+}
+func decodeDictionary(pos *int, bencodedString string) (map[string]any, error) {
+	var newDict map[string]any = make(map[string]any)
+	if len(bencodedString[*pos:]) < 2 {
+		return newDict, errors.New("decode error: invalid dictionary")
+	}
+	*pos++
+	var previous string
+	for *pos < len(bencodedString) {
+		if bencodedString[*pos] == 'e' {
+			return newDict, nil
+		}
+		if !(findOutBencodeType(rune(bencodedString[*pos])) == BencodeString) {
+			return newDict, errors.New("decode error: the key must be a string")
+		}
+		key, err := decodeString(pos, bencodedString)
+		if err != nil {
+			return newDict, err
+		}
+		if previous == "" {
+			previous = key
+		}
+		current := key
+		if previous > current {
+			return newDict, errors.New("decode error:the dictionary keys aren't lexicographically sorted")
+		}
+		*pos++
+		if *pos >= len(bencodedString) {
+			return newDict, errors.New("decode error: malformed dict")
+		}
+		value, err := _decodeBencode(pos, bencodedString)
+		if err != nil {
+			return newDict, err
+		}
+		newDict[key] = value
+		*pos++
+		previous = current
+	}
+	return newDict, errors.New("decode error: malformed dictionary")
+
+}
+func decodeList(pos *int, bencodedString string) ([]any, error) {
+	newList := make([]any, 0)
+	if len(bencodedString[*pos:]) < 2 {
+		return newList, errors.New("decode error: list length must be greater than 2")
+	}
+	*pos++
+	for *pos < len(bencodedString) {
+		if bencodedString[*pos] == 'e' {
+			return newList, nil
+		}
+		el, err := _decodeBencode(pos, bencodedString)
+		if err != nil {
+			return newList, err
+		}
+		newList = append(newList, el)
+		*pos++
+	}
+	return newList, errors.New("decode error: list malformed")
+}
+func decodeString(pos *int, bencodedString string) (string, error) {
+	if len(bencodedString) == 0 || len(bencodedString[*pos:]) == 0 {
+		return "", errors.New("decode error: ")
+	}
+	sizeFirstPos := *pos
+	for *pos < len(bencodedString) {
+		if bencodedString[*pos] != ':' {
+			*pos++
+			continue
+		}
+		if *pos == 0 || *pos+1 == len(bencodedString) {
+			break
+		}
+		length, err := strconv.Atoi(bencodedString[sizeFirstPos:*pos])
+		*pos++
+		if err != nil || length > len(bencodedString)-(*pos) {
+			break
+		}
+		start := *pos
+		end := *pos + length
+		*pos = end - 1
+		return bencodedString[start:end], nil
+	}
+	return "", errors.New("decode error: byte string couldn't be parsed")
+}
+
+func decodeInteger(pos *int, bencodedString string) (int, error) {
+	bencodedStringLength := len(bencodedString)
+	if err := validateMinimumIntegerLength(pos, bencodedString); err != nil {
+		return 0, err
+	}
+	*pos++
+	isNegative := bencodedString[*pos] == '-'
+	if isNegative && !isValidNegativeNumber(pos, bencodedString) {
+		return 0, errors.New("decode error: string composition isn't correct")
+	}
+	if isNegative {
+		*pos++
+	}
+	if !isNegative && bencodedString[*pos] == '0' && bencodedString[*pos+1] != 'e' {
+		return 0, errors.New("decode error: the composition i0xe isn't allowed")
+	}
+	start := *pos
+	for *pos < bencodedStringLength {
+		if bencodedString[*pos] == 'e' {
+			break
+		}
+		*pos++
+	}
+	if *pos >= bencodedStringLength {
+		return 0, errors.New("decode error: \"i\" termination character couldn't be found")
+	}
+	intValue, err := strconv.Atoi(string(bencodedString[start:*pos]))
+	if err != nil {
+		return 0, errors.New("decode error: malformed integer value")
+	}
+	if isNegative {
+		intValue = (-1) * intValue
+	}
+	return intValue, err
+}
+func validateMinimumIntegerLength(pos *int, bencodedString string) error {
+	if len(bencodedString) == 0 || len(bencodedString[*pos:]) < 3 {
+		return errors.New("decode error: integer value couldn't be decoded")
+	}
+	return nil
+}
+func isValidNegativeNumber(pos *int, bencodedString string) bool {
+	remaining := bencodedString[*pos:]
+	if len(remaining) < 3 {
+		return false
+	}
+	if remaining[1] == '0' {
+		return false
+	}
+	return true
+}
+
+// Example:
+// - 5:hello -> hello
+// - 10:hello12345 -> hello12345
+
+func findOutBencodeType(char rune) int {
+	if char == 'i' {
+		return BencodeInteger
+	} else if char == 'l' {
+		return BencodeList
+	} else if char == 'd' {
+		return BencodeDict
+	} else if unicode.IsDigit(char) {
+		return BencodeString
+	} else {
+		return BencodeInvalid
+	}
+}
--- app/main.go
+++ app/main.go
@@ -5,229 +5,51 @@ import (
 	"errors"
 	"fmt"
 	"os"
-	"strconv"
-	"unicode"
-	// bencode "github.com/jackpal/bencode-go" // Available if you need it!
-)
-
-const (
-	BencodeInteger = iota
-	BencodeList
-	BencodeDict
-	BencodeString
-	BencodeInvalid
 )
 
 // Ensures gofmt doesn't remove the "os" encoding/json import (feel free to remove this!)
 var _ = json.Marshal
-
-func decodeBencode(bencodedString string) (any, error) {
-	if len(bencodedString) == 0 {
-		return 0, errors.New("decode error: the string is empty")
-	}
-	var bencodeRunes []rune = []rune(bencodedString)
-	pos := 0
-	var value any
-	var err error
-	for pos < len(bencodeRunes) {
-		value, err = _decodeBencode(&pos, bencodeRunes)
-		if err != nil {
-			return value, err
-		}
-		pos++
-	}
-	return value, err
-}
-func _decodeBencode(pos *int, bencodeRunes []rune) (any, error) {
-	switch findOutBencodeType(bencodeRunes[*pos]) {
-	case BencodeString:
-		return decodeString(pos, bencodeRunes)
-	case BencodeInteger:
-		return decodeInteger(pos, bencodeRunes)
-	case BencodeList:
-		return decodeList(pos, bencodeRunes)
-	case BencodeDict:
-		return decodeDictionary(pos, bencodeRunes)
-	default:
-		break
-	}
-	return "", errors.New("decode error: invalid character")
-}
-func decodeDictionary(pos *int, bencodeRunes []rune) (map[string]any, error) {
-	var newDict map[string]any = make(map[string]any)
-	if len(bencodeRunes[*pos:]) < 2 {
-		return newDict, errors.New("decode error: invalid dictionary")
-	}
-	*pos++
-	var previous string
-	for *pos < len(bencodeRunes) {
-		if bencodeRunes[*pos] == 'e' {
-			return newDict, nil
-		}
-		if !(findOutBencodeType(bencodeRunes[*pos]) == BencodeString) {
-			return newDict, errors.New("decode error: the key must be a string")
-		}
-		key, err := decodeString(pos, bencodeRunes)
-		if err != nil {
-			return newDict, err
-		}
-		if previous == "" {
-			previous = key
-		}
-		current := key
-		if previous > current {
-			return newDict, errors.New("decode error:the dictionary keys aren't lexicographically sorted")
-		}
-		*pos++
-		if *pos >= len(bencodeRunes) {
-			return newDict, errors.New("decode error: malformed dict")
-		}
-		value, err := _decodeBencode(pos, bencodeRunes)
-		if err != nil {
-			return newDict, err
-		}
-		newDict[key] = value
-		*pos++
-		previous = current
-	}
-	return newDict, errors.New("decode error: malformed dictionary")
-
-}
-func decodeList(pos *int, bencodeRunes []rune) ([]any, error) {
-	newList := make([]any, 0)
-	if len(bencodeRunes[*pos:]) < 2 {
-		return newList, errors.New("decode error: list length must be greater than 2")
-	}
-	*pos++
-	for *pos < len(bencodeRunes) {
-		if bencodeRunes[*pos] == 'e' {
-			return newList, nil
-		}
-		el, err := _decodeBencode(pos, bencodeRunes)
-		if err != nil {
-			return newList, err
-		}
-		newList = append(newList, el)
-		*pos++
-	}
-	return newList, errors.New("decode error: list malformed")
-}
-func decodeString(pos *int, bencodeRunes []rune) (string, error) {
-	if len(bencodeRunes) == 0 || len(bencodeRunes[*pos:]) == 0 {
-		return "", errors.New("decode error: ")
-	}
-	sizeFirstPos := *pos
-	for *pos < len(bencodeRunes) {
-		if bencodeRunes[*pos] != ':' {
-			*pos++
-			continue
-		}
-		if *pos == 0 || *pos+1 == len(bencodeRunes) {
-			break
-		}
-		length, err := strconv.Atoi(string(bencodeRunes[sizeFirstPos:*pos]))
-		*pos++
-		if err != nil || length > len(bencodeRunes)-(*pos) {
-			break
-		}
-		start := *pos
-		end := *pos + length
-		returnString := string(bencodeRunes[start:end])
-		*pos = end - 1
-		return returnString, nil
-	}
-	return "", errors.New("decode error: byte string couldn't be parsed")
-}
-
-func decodeInteger(pos *int, bencodeRunes []rune) (int, error) {
-	bencodeRunesLength := len(bencodeRunes)
-	if err := validateMinimumIntegerLength(pos, bencodeRunes); err != nil {
-		return 0, err
-	}
-	*pos++
-	isNegative := bencodeRunes[*pos] == '-'
-	if isNegative && !isValidNegativeNumber(pos, bencodeRunes) {
-		return 0, errors.New("decode error: string composition isn't correct")
-	}
-	if isNegative {
-		*pos++
-	}
-	if !isNegative && bencodeRunes[*pos] == '0' && bencodeRunes[*pos+1] != 'e' {
-		return 0, errors.New("decode error: the composition i0xe isn't allowed")
-	}
-	start := *pos
-	for *pos < bencodeRunesLength {
-		if bencodeRunes[*pos] == 'e' {
-			break
-		}
-		*pos++
-	}
-	if *pos >= bencodeRunesLength {
-		return 0, errors.New("decode error: \"i\" termination character couldn't be found")
-	}
-	intValue, err := strconv.Atoi(string(bencodeRunes[start:*pos]))
-	if err != nil {
-		return 0, errors.New("decode error: malformed integer value")
-	}
-	if isNegative {
-		intValue = (-1) * intValue
-	}
-	return intValue, err
-}
-func validateMinimumIntegerLength(pos *int, bencodeRunes []rune) error {
-	if len(bencodeRunes) == 0 || len(bencodeRunes[*pos:]) < 3 {
-		return errors.New("decode error: integer value couldn't be decoded")
-	}
-	return nil
-}
-func isValidNegativeNumber(pos *int, bencodeRunes []rune) bool {
-	remaining := bencodeRunes[*pos:]
-	if len(remaining) < 3 {
-		return false
-	}
-	if remaining[1] == '0' {
-		return false
-	}
-	return true
-}
-
-// Example:
-// - 5:hello -> hello
-// - 10:hello12345 -> hello12345
-
-func findOutBencodeType(char rune) int {
-	if char == 'i' {
-		return BencodeInteger
-	} else if char == 'l' {
-		return BencodeList
-	} else if char == 'd' {
-		return BencodeDict
-	} else if unicode.IsDigit(char) {
-		return BencodeString
-	} else {
-		return BencodeInvalid
-	}
-}
 
 func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	fmt.Fprintln(os.Stderr, "Logs from your program will appear here!")
 
 	command := os.Args[1]
-
-	if command == "decode" {
-		bencodedValue := os.Args[2]
-
-		decoded, err := decodeBencode(bencodedValue)
+	arg2 := os.Args[2]
+	switch command {
+	case "decode":
+		decoded, err := decodeBencode(arg2)
 		if err != nil {
 			fmt.Println(err)
-			return
+			os.Exit(1)
 		}
-
 		jsonOutput, _ := json.Marshal(decoded)
 		fmt.Println(string(jsonOutput))
-	} else {
+	case "info":
+		data, err := os.ReadFile(arg2)
+		if err != nil {
+			fmt.Println("error: file couldn't be open")
+			os.Exit(1)
+		}
+		parsedData, err := parseTorrentFile(string(data))
+		if err != nil {
+			fmt.Println(err)
+		}
+		info := parsedData["info"].(map[string]any)
+		fmt.Printf("Tracker URL: %s\n", parsedData["announce"])
+		fmt.Printf("Length: %d", info["length"])
+	default:
 		fmt.Println("Unknown command: " + command)
 		os.Exit(1)
 	}
+}
+func parseTorrentFile(data string) (map[string]any, error) {
+	if findOutBencodeType(rune(data[0])) != BencodeDict {
+		return nil, errors.New("parse error: the .torrent file couldn't be parsed")
+	}
+	parsedInfo, err := decodeBencode(data)
+	if err != nil {
+		return nil, err
+	}
+	return parsedInfo.(map[string]any), nil
 }
